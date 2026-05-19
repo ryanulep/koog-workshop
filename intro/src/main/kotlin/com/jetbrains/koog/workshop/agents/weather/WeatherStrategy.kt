3a84038f110f9705a8481fd61b89b509c5f3d991
svetlana.isakova <svetlana.isakova@jetbrains.com>
2026-05-19 11:11:30 +0200

    Using new single run strategy implementation in the tasks

--- intro/src/main/kotlin/com/jetbrains/koog/workshop/agents/weather/WeatherStrategy.kt
+++ intro/src/main/kotlin/com/jetbrains/koog/workshop/agents/weather/WeatherStrategy.kt
@@ -8,30 +8,16 @@ import ai.koog.prompt.message.Message
 /**
  * Low-level implementation of the basic loop strategy by hand.
  */
-fun basicSingleRunStrategyByHandSimplerVersion() = strategy("single-run-strategy-by-hand") {
-    val callLLm: AIAgentNodeBase<Message.User, Message.Assistant> by nodeLLMRequest()
-    val executeTools: AIAgentNodeBase<ToolCalls, Message.User> by nodeExecuteTools()
-
-    edge(nodeStart forwardTo callLLm asUserMessage { it })
-    edge(callLLm forwardTo executeTools onToolCalls { true })
-    edge(callLLm forwardTo nodeFinish onTextMessage { true })
-    edge(executeTools forwardTo callLLm)
-}
-
-/**
- * Low-level implementation of the basic loop strategy by hand.
- */
-fun basicSingleRunStrategyByHand() = strategy("single-run-strategy-by-hand") {
+fun basicSingleRunStrategyByHand() = strategy<String, String>("single-run-strategy-by-hand") {
     val nodeCallLLM: AIAgentNodeBase<Message.User, Message.Assistant> by nodeLLMRequest()
-    val nodeExecuteTool: AIAgentNodeBase<ToolCalls, ReceivedToolResults> by nodeExecuteToolsAndGetResults()
-    val nodeSendToolResult: AIAgentNodeBase<ReceivedToolResults, Message.Assistant> by nodeLLMSendToolResults()
+    val nodeExecuteTools: AIAgentNodeBase<ToolCalls, Message.User> by nodeExecuteTools()
 
+    // Replace the following with the default strategy implementation:
+//    edge(nodeStart forwardTo nodeFinish)
+
+    // Solution:
     edge(nodeStart forwardTo nodeCallLLM asUserMessage { it })
-    edge(nodeCallLLM forwardTo nodeExecuteTool onToolCalls { true })
+    edge(nodeCallLLM forwardTo nodeExecuteTools onToolCalls { true })
+    edge(nodeExecuteTools forwardTo nodeCallLLM)
     edge(nodeCallLLM forwardTo nodeFinish onTextMessage { true })
-
-    // Task: Complete the strategy implementation.
-    edge(nodeExecuteTool forwardTo nodeSendToolResult)
-    edge(nodeSendToolResult forwardTo nodeFinish onTextMessage { true })
-    edge(nodeSendToolResult forwardTo nodeExecuteTool onToolCalls { true })
 }
